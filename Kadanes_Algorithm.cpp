0b889f97c2f5c3319736e0c4503c6f3721b1a93b
Abha Himani <74365827+AbhaHimani@users.noreply.github.com>
2021-07-25 17:29:44 +0530

    Add files via upload

---
+++ Kadanes_Algorithm.cpp
@@ -0,0 +1,22 @@
+//O(N)
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int cs = 0;
+    int ms = 0;
+    int a[1000];
+    for (int i = 0; i < n; i++)
+    {
+        cs = cs + a[i];
+        if (cs < 0)
+        {
+            cs = 0;
+                }
+        ms = max(ms, cs);
+    }
+}
